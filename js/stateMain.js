9878611b204943f49cbb14b817aa13d686e405a9
rayGreycloud <rayGreycloud@gmail.com>
2017-07-06 10:35:17 -0700

    Add statemain to markup

---
+++ js/stateMain.js
@@ -0,0 +1,21 @@
+var StateMain = {
+  preload: function () {
+    // load assets
+
+  },
+
+  create: function () {
+    console.log('Ready!');
+    // set up objects, variables
+
+    // sounds, text
+
+    // characters, explosions
+
+  },
+  
+  update: function () {
+    // constant loop
+
+  }
+}
